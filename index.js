492bd88971942ab2b2409b55ad90c9444ce9b33d
Teanna Taylor <teannamtaylor95@gmail.com>
2022-05-26 07:05:06 -0700

    commit tree f5351f0c9f5a6e4cf59d79253d89cbf6a377e91b

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// declare a function called divide; divides 2000 by 100
+
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+
+divide(2000,100);
+
+// declare an arrow function called square
+
+const square = para1 => para1 * para1;
+
+square(4);
+
+//declare an arrow function called add 
+
+const add = (num1, num2) => num1 + num2;
+
+add(4,5);
